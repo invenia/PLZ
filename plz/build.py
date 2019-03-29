ba53426c7121733ac5782d2809c94480d2f14aba
Fernando Chorney <fernando.chorney@invenia.ca>
2019-03-29 09:50:57 -0500

    Re-structure build.py into smaller functions

--- plz/build.py
+++ plz/build.py
@@ -44,13 +44,8 @@ def build_package(
             already exists.
     """
     zipfile = build / "package.zip"
-
-    if requirements is None:
-        requirements = ()
-    elif isinstance(requirements, Path):
-        requirements = (requirements,)
-
     build_info = build / "build-info.json"
+    package = build / "package"
 
     try:
         info = {
@@ -67,76 +62,19 @@ def build_package(
         else:
             old_info = {}
 
-        if force or info != old_info:
-            package = build / "package"
+        # Just return the zipfile path, if there is no new work to do on it
+        if not force and info == old_info:
+            return zipfile
 
-            build.mkdir(parents=True, exist_ok=True)
-            with build_info.open("w") as stream:
-                json.dump(info, stream)
+        # Copy the included files into the package dir
+        copy_included_files(build, build_info, info, package, *files)
 
-            if package.exists():
-                rmtree(package)
+        # Process requirements files if they exist
+        if requirements:
+            process_requirements(requirements, build, package)
 
-            package.mkdir()
-
-            for path in files:
-                destination = package / path.name
-
-                if path.is_dir():
-                    subprocess.run(("cp", "-Rn", f"{path}", str(destination)))
-                else:
-                    copy2(str(path), str(destination))
-
-            if requirements:
-                client = docker.APIClient()
-                env = build / "package-env"
-                container = "plz-container"
-                build_docker_image(client, env)
-                start_docker_container(client, container, env)
-
-                # pip install all requirements files
-                for r_file in requirements:
-                    with r_file.open() as f:
-                        for line in f.readlines():
-                            dep = line.strip()
-                            pip_install(client, container, dep)
-
-                stop_docker_container(client, container)
-
-                # Remove all unnecessary __pycache__ dirs
-                for cache_dir in env.glob("**/__pycache__"):
-                    rmtree(cache_dir)
-
-                # Remove all unnecessary *.dist-info dirs
-                for dist_dir in env.glob("**/*.dist-info"):
-                    rmtree(dist_dir)
-
-                # Copy the python libs to the package
-                for path in env.iterdir():
-                    destination = package / path.name
-                    if not destination.exists():
-                        logging.debug(f"Copying {destination}")
-                        if path.is_dir():
-                            subprocess.run(("cp", "-Rn", f"{path}", str(destination)))
-                        else:
-                            copy2(str(path), str(destination))
-
-            with ZipFile(zipfile, "w") as z:
-                directories = [package]
-
-                while directories:
-                    directory = directories.pop()
-
-                    for path in directory.iterdir():
-                        if path.is_dir():
-                            if path.name != "__pycache__":
-                                directories.append(path)
-                        else:
-                            destination = path.relative_to(package)
-                            if zipped_prefix:
-                                destination = zipped_prefix / destination
-                            logging.debug(f"Archiving {path} to {destination}")
-                            z.write(path, destination)
+        # Zip it all up
+        zip_package(zipfile, package, zipped_prefix=zipped_prefix)
     except BaseException:
         # If _anything_ goes wrong, we need to rebuild, so kill
         # build_info.
@@ -146,3 +84,90 @@ def build_package(
         raise
 
     return zipfile
+
+
+def copy_included_files(
+    build_path: Path,
+    build_info: Path,
+    info: dict,
+    package_path: Path,
+    *files: Path,
+):
+    build_path.mkdir(parents=True, exist_ok=True)
+    with build_info.open("w") as stream:
+        json.dump(info, stream)
+
+    if package_path.exists():
+        rmtree(package_path)
+
+    package_path.mkdir()
+
+    for path in files:
+        destination = package_path / path.name
+
+        if path.is_dir():
+            subprocess.run(("cp", "-Rn", f"{path}", str(destination)))
+        else:
+            copy2(str(path), str(destination))
+
+
+def process_requirements(
+    requirements: Union[Path, Sequence[Path]],
+    build_path: Path,
+    package_path: Path,
+):
+    client = docker.APIClient()
+    env = build_path / "package-env"
+    container = "plz-container"
+    build_docker_image(client, env)
+    start_docker_container(client, container, env)
+
+    # pip install all requirements files
+    for r_file in requirements:
+        with r_file.open() as f:
+            for line in f.readlines():
+                dep = line.strip()
+                pip_install(client, container, dep)
+
+    stop_docker_container(client, container)
+
+    # Remove all unnecessary __pycache__ dirs
+    for cache_dir in env.glob("**/__pycache__"):
+        rmtree(cache_dir)
+
+    # Remove all unnecessary *.dist-info dirs
+    for dist_dir in env.glob("**/*.dist-info"):
+        rmtree(dist_dir)
+
+    # Copy the python libs to the package
+    for path in env.iterdir():
+        destination = package_path / path.name
+        if not destination.exists():
+            logging.debug(f"Copying {destination}")
+            if path.is_dir():
+                subprocess.run(("cp", "-Rn", f"{path}", str(destination)))
+            else:
+                copy2(str(path), str(destination))
+
+
+def zip_package(
+    zipfile_path: Path,
+    package_path: Path,
+    zipped_prefix: Optional[Path] = None
+):
+    with ZipFile(zipfile_path, "w") as z:
+        directories = [package_path]
+
+        while directories:
+            directory = directories.pop()
+
+            for path in directory.iterdir():
+                if path.is_dir():
+                    if path.name != "__pycache__":
+                        directories.append(path)
+                else:
+                    destination = path.relative_to(package_path)
+                    if zipped_prefix:
+                        destination = zipped_prefix / destination
+                    logging.debug(f"Archiving {path} to {destination}")
+                    z.write(path, destination)
